635006d0ec0fb409fdd7850204131fb14c6ddafc
Lance Choo <mr.longzhentian@gmail.com>
2026-03-19 11:25:00 +0800

    feat: add color parameter for customizable image background

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ app.use("/favicon.ico", serveStatic({ path: "./public/picsum.svg" }));
 
 app.get("/:width/:height?", async (c) => {
   const { width, height } = c.req.param();
-  const { text } = c.req.query();
+  const { text, color } = c.req.query();
 
   try {
     const imageBuffer = await sharp({
@@ -19,9 +19,15 @@ app.get("/:width/:height?", async (c) => {
         height: Number(height || width),
         channels: 3,
         background: {
-          r: Math.floor(Math.random() * 256),
-          g: Math.floor(Math.random() * 256),
-          b: Math.floor(Math.random() * 256),
+          r: color
+            ? parseInt(color.slice(1, 3), 16)
+            : Math.floor(Math.random() * 256),
+          g: color
+            ? parseInt(color.slice(3, 5), 16)
+            : Math.floor(Math.random() * 256),
+          b: color
+            ? parseInt(color.slice(5, 7), 16)
+            : Math.floor(Math.random() * 256),
         },
       },
     })
@@ -29,7 +35,7 @@ app.get("/:width/:height?", async (c) => {
         {
           input: Buffer.from(
             `<svg width="${width}" height="${height || width}">
-            <text x="50%" y="50%" font-size="32" fill="white" font-weight="bold" text-anchor="middle" dominant-baseline="middle">${text || `${width}*${height || width}`}</text>
+            <text x="50%" y="50%" font-size="32" fill="white" font-family="Arial" font-weight="bold" text-anchor="middle" dominant-baseline="middle">${text || `${width}*${height || width}`}</text>
           </svg>`,
           ),
           gravity: "center",
@@ -43,6 +49,7 @@ app.get("/:width/:height?", async (c) => {
       "Cache-Control": "public, max-age=86400",
     });
   } catch {
+    // console.log("Error processing image:", error);
     return c.text("Image processing failed.", 500);
   }
 });
